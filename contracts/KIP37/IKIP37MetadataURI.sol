e979d52369e8857a1ee7330a7d1c3f8ea1a83e45
keunhan lee <restart.doodoo@gmail.com>
2021-12-27 18:32:11 +0900

    :chore Remove kip 37

--- contracts/KIP37/IKIP37MetadataURI.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.5.6;
-
-import "./IKIP37.sol";
-
-contract IKIP37MetadataURI is IKIP37 {
-  function uri(uint256 id) external view returns (string memory);
-}
